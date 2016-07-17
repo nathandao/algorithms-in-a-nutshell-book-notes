fddf8248e079da32fd69a18d36328fe3da46a0d2
Nathan Dao <nathan@guynathan.com>
2016-07-17 21:00:20 +0700

    depth first search

---
+++ c6_graph/src/graph/depth_first_search.go
@@ -0,0 +1,61 @@
+package main
+
+// Search for a path from vs to vt and return an array that containes the
+// string node of each intersection.
+func DepthFirstSearch(g map[string][]string, vs, vt string) (bool, []string) {
+
+	// Initiate pred and color array with empty strings and default color WHITE.
+	color := make(map[string]string, len(g))
+	pred := make(map[string]string, len(g))
+	for v := range g {
+		pred[v] = ""
+		color[v] = WHITE
+	}
+
+	// Run the visit and store the path from vs to vt in pred.
+	dfsVisit(g, vs, vt, color, pred)
+
+	// Construct a reverse path array from each node's pred.
+	path := []string{vt}
+	current_node := vt
+	for current_node != vs && pred[current_node] != "" {
+		path = append(path, pred[current_node])
+		current_node = pred[current_node]
+	}
+
+	// Reverse the path array to get a direction from vs to vt.
+	return_path := []string{}
+	for i := len(path) - 1; i >= 0; i-- {
+		return_path = append(return_path, path[i])
+	}
+
+	// If the first item in path is vs, a path is found, otherwise, there is no
+	// link between vs and vt. Thus, no path.
+	found_path := true
+	if return_path[0] != vs {
+		found_path = false
+	}
+
+	return found_path, return_path
+}
+
+// dfsVisit recursively visits all possible paths from branch u, until the
+// destination t is reached. Branch is marked with color WHITE = unvisited,
+// GRAY = processing and BLACK = visited.
+func dfsVisit(g map[string][]string, u, t string, color, pred map[string]string) {
+	// Processing.
+	color[u] = GRAY
+
+	for _, v := range g[u] {
+		// Recursive visit if branch has not been visited.
+		if color[v] == WHITE {
+			pred[v] = u
+			// Only continue if target has not been reached.
+			if v != t {
+				dfsVisit(g, v, t, color, pred)
+			}
+		}
+	}
+
+	color[u] = BLACK
+}
---
+++ c6_graph/src/graph/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const WHITE = "WHITE"
+const BLACK = "BLACK"
+const GRAY = "GRAY"
+
+func main() {
+	G := map[string][]string{
+		"A": []string{"B", "C", "D", "F"},
+		"B": []string{"A", "D", "G"},
+		"C": []string{"A", "D"},
+		"D": []string{"A", "B", "E"},
+		"E": []string{"A", "D", "H"},
+		"F": []string{"A", "G"},
+		"G": []string{"B", "F", "H"},
+		"H": []string{"G", "E"},
+		"I": []string{},
+	}
+
+	fmt.Println("Undirected unweighted graph:")
+	for k, v := range G {
+		fmt.Println(k, ":", v)
+	}
+	fmt.Println("\n**NOTE: SEE IMAGE './graph.png' FOR VISUAL PRESENTATION OF THE GRAPH\n")
+
+	fmt.Println("=== DEPTH FIRST SEARCH ===")
+
+	found, pred := DepthFirstSearch(G, "A", "H")
+	fmt.Println(" Path exists from A to H:", found, ", path:", pred)
+
+	found, pred = DepthFirstSearch(G, "G", "B")
+	fmt.Println(" Path exists from G to B:", found, ", path:", pred)
+
+	found, pred = DepthFirstSearch(G, "A", "I")
+	fmt.Println(" Path exists from A to I:", found, ", path:", pred)
+}
